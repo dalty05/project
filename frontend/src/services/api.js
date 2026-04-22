86228b0fd2ef4c5b80b68b39a63741a8ce221b2e
dalty05 <oyigodalton@gmail.com>
2026-04-22 16:24:36 +0300

    Change API base URL to production server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create a custom axios instance
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: "https://daltyo5.pythonanywhere.com/",
   headers: {
     "Content-Type": "application/json",
   },
